eb2e50ff546c08d11879e6d44c71d985b82747df
simon <stelter@tzi.de>
2018-03-05 12:48:45 +0100

    added some todos to object.py

--- src/giskardpy/object.py
+++ src/giskardpy/object.py
@@ -1,43 +1,14 @@
 from giskardpy.trajectory import Transform
 
+
 class URDFSerializationInterface(object):
+    #TODO init that sets the function parameters
     def to_xml_string(self):
         pass
 
-    def from_xml_string(self):
+    def from_xml_string(self, xml_string):
         pass
 
-class InertiaMatrix(URDFSerializationInterface):
-    ixx = 0.0
-    ixy = 0.0
-    ixz = 0.0
-    iyy = 0.0
-    iyz = 0.0
-    izz = 0.0
-
-class InertialProperties(URDFSerializationInterface):
-    origin = Transform()
-    mass = 0.0
-    inertia = InertiaMatrix()
-
-class GeometricShape(URDFSerializationInterface):
-    pass
-
-class BoxShape(GeometricShape):
-    x = 0.0
-    y = 0.0
-    z = 0.0
-
-class CylinderShape(GeometricShape):
-    radius = 0.0
-    length = 0.0
-
-class SphereShape(GeometricShape):
-    radius = 0.0
-
-class MeshShape(GeometricShape):
-    filename = ''
-    scale = [1.0, 1.0, 1.0]
 
 class ColorRgba(URDFSerializationInterface):
     r = 1.0
@@ -45,24 +16,67 @@ class ColorRgba(URDFSerializationInterface):
     b = 1.0
     a = 1.0
 
-class MaterialProperties(URDFSerializationInterface):
+
+class InertiaMatrix(URDFSerializationInterface):
+    ixx = 0.0  # TODO 1?
+    ixy = 0.0
+    ixz = 0.0
+    iyy = 0.0  # TODO 1?
+    iyz = 0.0
+    izz = 0.0  # TODO 1?
+
+
+class GeometricShape(URDFSerializationInterface):
+    pass
+
+
+class BoxShape(GeometricShape):
+    x = 0.0
+    y = 0.0
+    z = 0.0
+
+
+class CylinderShape(GeometricShape):
+    radius = 0.0
+    length = 0.0
+
+
+class SphereShape(GeometricShape):
+    radius = 0.0
+
+
+class MeshShape(GeometricShape):
+    filename = ''
+    scale = [1.0, 1.0, 1.0]
+
+
+class InertialProperty(URDFSerializationInterface):
+    origin = Transform()
+    mass = 0.0
+    inertia = InertiaMatrix()
+
+
+class MaterialProperty(URDFSerializationInterface):
     name = ''
     color = ColorRgba()
     texture_filename = ''
 
-class VisualProperties(URDFSerializationInterface):
+
+class VisualProperty(URDFSerializationInterface):
     name = ''
     origin = Transform()
     geometry = None
     material = None
 
-class CollisionProperties(URDFSerializationInterface):
+
+class CollisionProperty(URDFSerializationInterface):
     name = ''
     origin = Transform()
     geometry = None
 
+
 class WorldObject(URDFSerializationInterface):
     name = ''
-    inertial_props = InertialProperties()
+    inertial_props = InertialProperty()
     visual_props = []
     collision_props = []

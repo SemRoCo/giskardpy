ec47a31bc6291426abd3c269fdcef886b6b73ac7
kevin <s_bo8dc2@uni-bremen.de>
2019-10-09 14:24:48 +0200

    adjusted hsr launch files

--- scripts/hsrb_urdf_publisher.py
+++ scripts/hsrb_urdf_publisher.py
@@ -3,22 +3,23 @@ import rospy
 from giskardpy.urdf_object import URDFObject
 from geometry_msgs.msg import Pose
 from giskardpy import logging
+import rospkg
 
 
 if not rospy.has_param('/robot_description'):
     logging.loginfo('waiting for rosparam /robot_description')
-    rospy.sleep(0.1)
+    rospy.sleep(1.0)
 
+rospack = rospkg.RosPack()
 
 hsrb_urdf = rospy.get_param('/robot_description')
-#hsrb_urdf = hsrb_urdf.replace('hsrb_description', 'hsr_description')
-#hsrb_urdf = hsrb_urdf.replace('hsrb_parts_description', 'hsr_description')
 hsrb = URDFObject(hsrb_urdf)
-hsrb = URDFObject.from_urdf_file('/home/kevin/Documents/catkin_ws/src/hsr_description/robots/hsrb4s.urdf')
-hsrb_base = URDFObject.from_urdf_file('/home/kevin/Documents/catkin_ws/src/giskardpy/test/urdfs/hsr_base.urdf')
+#hsrb_urdf_path = rospack.get_path('hsr_description') + '/robots/hsrb4s.urdf'
+#hsrb = URDFObject.from_urdf_file(hsrb_urdf_path)
+hsr_base_urdf_path = rospack.get_path('giskardpy') + '/test/urdfs/hsr_base.urdf'
+hsrb_base = URDFObject.from_urdf_file(hsr_base_urdf_path )
 
 hsrb_base.attach_urdf_object(hsrb, 'odom_t_link', Pose())
 hsrb_with_base_urdf = hsrb_base.get_urdf_str()
 
 rospy.set_param('/giskard/robot_description', hsrb_with_base_urdf)
-#rospy.set_param('/robot_description', hsrb_with_base_urdf)

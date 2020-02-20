d09548ebeef9eddb05efc8105d722ee2461d1a91
kevin <s_bo8dc2@uni-bremen.de>
2020-02-20 15:52:14 +0100

    improved logging 2

--- src/giskardpy/logging.py
+++ src/giskardpy/logging.py
@@ -2,21 +2,6 @@ import rospy
 from inspect import currentframe, getframeinfo
 from giskardpy import identifier
 
-def generate_debug_msg(msg):
-    node_name = rospy.get_name()
-    frameinfo = getframeinfo(currentframe().f_back.f_back)
-    file_info = frameinfo.filename.split('/')[-1] + ' line ' + str(frameinfo.lineno)
-    new_msg = ('\nnode: ' + node_name + '\n' +
-               'file: ' + file_info + '\n' +
-               'message: ' + msg + '\n')
-    return new_msg
-
-def generate_msg(msg):
-    node_name = rospy.get_name()
-    new_msg = '[' + node_name + ']' + ': ' + msg
-    return new_msg
-
-
 
 def debug():
     if debug.param == None:
@@ -29,35 +14,43 @@ def debug():
 
 debug.param = None
 
+
+def generate_debug_msg(msg):
+    node_name = rospy.get_name()
+    frameinfo = getframeinfo(currentframe().f_back.f_back)
+    file_info = frameinfo.filename.split('/')[-1] + ' line ' + str(frameinfo.lineno)
+    new_msg = ('\nnode: ' + node_name + '\n' +
+               'file: ' + file_info + '\n' +
+               'message: ' + msg + '\n')
+    return new_msg
+
+
+def generate_msg(msg):
+    if(debug()):
+        return generate_debug_msg(msg)
+    else:
+        node_name = rospy.get_name()
+        new_msg = '[' + node_name + ']' + ': ' + msg
+        return new_msg
+
+
 def logdebug(msg):
     final_msg = generate_debug_msg(msg)
     rospy.logdebug(final_msg)
 
 def loginfo(msg):
-    if (debug()):
-        final_msg = generate_debug_msg(msg)
-    else:
-        final_msg = generate_msg(msg)
+    final_msg = generate_msg(msg)
     rospy.loginfo(final_msg)
 
 def logwarn(msg):
-    if (debug()):
-        final_msg = generate_debug_msg(msg)
-    else:
-        final_msg = generate_msg(msg)
+    final_msg = generate_msg(msg)
     rospy.logwarn(final_msg)
 
 def logerr(msg):
-    if (debug()):
-        final_msg = generate_debug_msg(msg)
-    else:
-        final_msg = generate_msg(msg)
+    final_msg = generate_msg(msg)
     rospy.logerr(final_msg)
 
 def logfatal(msg):
-    if (debug()):
-        final_msg = generate_debug_msg(msg)
-    else:
-        final_msg = generate_msg(msg)
+    final_msg = generate_msg(msg)
     rospy.logfatal(final_msg)
 

ada89609e1788bfb71ab72980ac0970a57bb5f10
kevin <s_bo8dc2@uni-bremen.de>
2020-02-20 15:40:57 +0100

    improved logging

--- src/giskardpy/logging.py
+++ src/giskardpy/logging.py
@@ -1,17 +1,63 @@
 import rospy
+from inspect import currentframe, getframeinfo
+from giskardpy import identifier
+
+def generate_debug_msg(msg):
+    node_name = rospy.get_name()
+    frameinfo = getframeinfo(currentframe().f_back.f_back)
+    file_info = frameinfo.filename.split('/')[-1] + ' line ' + str(frameinfo.lineno)
+    new_msg = ('\nnode: ' + node_name + '\n' +
+               'file: ' + file_info + '\n' +
+               'message: ' + msg + '\n')
+    return new_msg
+
+def generate_msg(msg):
+    node_name = rospy.get_name()
+    new_msg = '[' + node_name + ']' + ': ' + msg
+    return new_msg
+
+
+
+def debug():
+    if debug.param == None:
+        l = identifier.debug
+        param_name = '/giskard/' + '/'.join(s for s in l[1:])
+        debug.param = rospy.get_param(param_name)
+        return debug.param
+    else:
+        return debug.param
+
+debug.param = None
 
 def logdebug(msg):
-    rospy.logdebug(msg)
+    final_msg = generate_debug_msg(msg)
+    rospy.logdebug(final_msg)
 
 def loginfo(msg):
-    rospy.loginfo(msg)
+    if (debug()):
+        final_msg = generate_debug_msg(msg)
+    else:
+        final_msg = generate_msg(msg)
+    rospy.loginfo(final_msg)
 
 def logwarn(msg):
-    rospy.logwarn(msg)
+    if (debug()):
+        final_msg = generate_debug_msg(msg)
+    else:
+        final_msg = generate_msg(msg)
+    rospy.logwarn(final_msg)
 
 def logerr(msg):
-    rospy.logerr(msg)
+    if (debug()):
+        final_msg = generate_debug_msg(msg)
+    else:
+        final_msg = generate_msg(msg)
+    rospy.logerr(final_msg)
 
 def logfatal(msg):
-    rospy.logfatal(msg)
+    if (debug()):
+        final_msg = generate_debug_msg(msg)
+    else:
+        final_msg = generate_msg(msg)
+    rospy.logfatal(final_msg)
 

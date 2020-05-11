49d3fd9a7519d600cdf44c91e528c14378e1ea8c
kevin <s_bo8dc2@uni-bremen.de>
2020-05-11 15:03:03 +0200

    better plot formatting

--- src/giskardpy/utils.py
+++ src/giskardpy/utils.py
@@ -333,11 +333,13 @@ def create_path(path):
                 raise
 
 
-def plot_trajectory(tj, controlled_joints, path_to_data_folder, sample_period, order=3):
+def plot_trajectory(tj, controlled_joints, path_to_data_folder, sample_period, order=3, velocity_threshold=0.0, scaling=0.2):
     """
     :type tj: Trajectory
     :param controlled_joints: only joints in this list will be added to the plot
     :type controlled_joints: list
+    :param velocity_threshold: only joints that exceed this velocity threshold will be added to the plot. Use a negative number if you want to include every joint
+    :param scaling: this multiplier scales the space between trajectory points in the graph
     """
     order = max(order, 2)
     if len(tj._points) <= 0:
@@ -361,29 +363,32 @@ def plot_trajectory(tj, controlled_joints, path_to_data_folder, sample_period, o
         data[i] = np.diff(data[i - 1], axis=0, prepend=0)
     times = np.array(times) * sample_period
 
-    f, axs = plt.subplots(order, sharex=True)
-    f.set_size_inches(w=6, h=order * 1.5)
+    f, axs = plt.subplots(order, sharex=True, gridspec_kw={'hspace': 0.3})
+    f.set_size_inches(w=len(tj._points) * scaling, h=order * 1.5)
+
+    plt.xlim(times[0], times[-1])
 
     for i in range(order):
         if i == 0:
             axs[i].set_title(r'$p$')
         else:
-            axs[i].set_title(r'$\d' + 'd' * (i - 1) + 'ot{p}$')
+            axs[i].set_title(r'$p' + '\'' * i + "$")
     for i in range(len(controlled_joints)):
-        for j in range(order):
-            axs[j].plot(times, data[j][:, i], fmts[i], label=names[i])
+        if any(abs(data[1][:, i]) > velocity_threshold):
+            for j in range(order):
+                    axs[j].plot(times, data[j][:, i], fmts[i], label=names[i])
 
     for i in range(order):
         box = axs[i].get_position()
         axs[i].set_position([box.x0, box.y0, box.width * 0.7, box.height * 0.95])
 
-    # Put a legend to the right of the current axis
-    axs[0].legend(loc=u'right', bbox_to_anchor=(1.6, -0.1), prop={'size': 8})
+    axs[0].legend(bbox_to_anchor=(1.01, 1), loc='upper left')
+
     axs[-1].set_xlabel(u'time [s]')
     for i in range(order):
         axs[i].grid()
 
-    plt.savefig(path_to_data_folder + u'trajectory.pdf')
+    plt.savefig(path_to_data_folder + u'trajectory.pdf', bbox_inches="tight")
 
 
 def resolve_ros_iris_in_urdf(input_urdf):

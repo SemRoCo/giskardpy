bf5eb66cce6e95ce0f677fa0be31343ebd07aa9b
Thomas Lipps <48207686+tlipps@users.noreply.github.com>
2021-04-30 11:45:29 +0200

    Update test.sh

--- entrypoints/test.sh
+++ entrypoints/test.sh
@@ -2,8 +2,4 @@
 
 source ~/ros_ws/devel/setup.bash
 cd ~/ros_ws/src/giskardpy
-gnome-terminal --command="roslaunch iai_kitchen upload_kitchen_obj.launch"
-sleep 5
-gnome-terminal --command="roslaunch iai_pr2_sim ros_control_sim_with_base.launch"
-sleep 5
 pytest test/test_integration_pr2.py

43ebac6524f572a3e069209c84ca8c925379a29a
Simon Stelter <stelter@uni-bremen.de>
2021-08-09 14:26:37 +0000

    fixed self collision avoidance

--- src/giskardpy/constraints.py
+++ src/giskardpy/constraints.py
@@ -1025,7 +1025,7 @@ class CartesianPositionStraight(BasicCartesianGoal):
                                                          root_P_goal)
         # Constraint to stick to the line
         self.add_minimize_position_constraints(r_P_g=nearest,
-                                               max_velocity=max_velocity,
+                                               reference_velocity=max_velocity,
                                                root=self.root,
                                                tip=self.tip,
                                                prefix=u'line',
@@ -1664,25 +1664,24 @@ class SelfCollisionAvoidance(Goal):
                                  w.min(number_of_self_collisions, num_repeller))
 
         penetration_distance = soft_threshold - actual_distance
-        lower_limit = penetration_distance
-        upper_limit = 1e2
-
-        # I have to consider, that the qp controller limits velocities
         qp_limits_for_lba = max_velocity * sample_period * self.control_horizon
 
-        # this has to take into account the inner workings of the qp controller to compute an accurate slack limit
+        penetration_distance = soft_threshold - actual_distance
+        lower_limit = penetration_distance
+
         lower_limit_limited = w.limit(lower_limit,
                                       -qp_limits_for_lba,
                                       qp_limits_for_lba)
-        lower_limit_for_hard_threshold = w.limit(hard_threshold - actual_distance,
-                                                 -qp_limits_for_lba,
-                                                 qp_limits_for_lba)
+
         upper_slack = w.if_greater(actual_distance, hard_threshold,
-                                   -lower_limit_for_hard_threshold + lower_limit_limited,
+                                   w.limit(soft_threshold - hard_threshold,
+                                           -qp_limits_for_lba,
+                                           qp_limits_for_lba),
                                    lower_limit_limited)
 
         # undo factor in A
         upper_slack /= (sample_period * self.prediction_horizon)
+        # upper_slack *= 10
 
         upper_slack = w.if_greater(actual_distance, 50,  # assuming that distance of unchecked closest points is 100
                                    1e4,
@@ -1692,7 +1691,7 @@ class SelfCollisionAvoidance(Goal):
         self.add_constraint(u'/position',
                             reference_velocity=max_velocity,
                             lower_error=lower_limit,
-                            upper_error=upper_limit,
+                            upper_error=100,
                             weight=weight,
                             expression=dist,
                             lower_slack_limit=-1e4,

2e006cc3ff5e2cc13c0d0df749128c5d124c2e9d
Simon Stelter <stelter@uni-bremen.de>
2021-09-09 13:32:05 +0000

    updated saved_pandas

--- src/giskardpy/qp/qp_controller.py
+++ src/giskardpy/qp/qp_controller.py
@@ -13,22 +13,21 @@ from giskardpy.exceptions import OutOfJointLimitsException, \
     HardConstraintsViolatedException
 from giskardpy.qp.constraint import VelocityConstraint, Constraint
 from giskardpy.qp.free_variable import FreeVariable
-from giskardpy.utils.utils import memoize, logging
+from giskardpy.utils.utils import memoize, logging, create_path
 
 
 def save_pandas(dfs, names, path):
-    file_name = u'{}/pandas_{}'.format(path, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    folder_name = '{}/pandas_{}/'.format(path, datetime.datetime.now().strftime('%Yy-%mm-%dd--%Hh-%Mm-%Ss'))
+    create_path(folder_name)
     for df, name in zip(dfs, names):
         csv_string = u'name\n'
-        # csv_string += u'{}\n'.format(name)
         with pd.option_context('display.max_rows', None, 'display.max_columns', None):
             if df.shape[1] > 1:
                 for column_name, column in df.T.items():
                     csv_string += column.add_prefix(column_name + u'||').to_csv()
             else:
                 csv_string += df.to_csv()
-        # csv_string += u'----------------------------------------------------------------------------'
-        file_name2 = '{}_{}.csv'.format(file_name, name)
+        file_name2 = '{}{}.csv'.format(folder_name, name)
         with open(file_name2, 'w') as f:
             f.write(csv_string)
 
@@ -642,11 +641,11 @@ class QPController(object):
         if self.p_xdot is not None:
             save_pandas([self.p_weights, self.p_A, self.p_lbA, self.p_ubA, self.p_lb, self.p_ub, self.p_xdot],
                         ['weights', 'A', 'lbA', 'ubA', 'lb', 'ub', 'xdot'],
-                        u'tmp_data')
+                        u'../tmp_data')
         else:
             save_pandas([self.p_weights, self.p_A, self.p_lbA, self.p_ubA, self.p_lb, self.p_ub],
                         ['weights', 'A', 'lbA', 'ubA', 'lb', 'ub'],
-                        u'tmp_data')
+                        u'../tmp_data')
 
     def __is_nan_in_array(self, name, p_array):
         p_filtered = p_array.apply(lambda x: zip(x.index[x.isnull()].tolist(), x[x.isnull()]), 1)

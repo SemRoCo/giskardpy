35181115ed619c8de0a54b824cc175a62d04dfcd
Simon Stelter <stelter@uni-bremen.de>
2021-12-13 14:20:50 +0000

    moved grow tree functions into their own files

--- src/giskardpy/garden.py
+++ src/giskardpy/garden.py
@@ -1,61 +1,18 @@
 from collections import defaultdict
 
 import rospy
-from py_trees import Sequence, Selector, BehaviourTree, Blackboard
-from py_trees.meta import failure_is_success, success_is_failure, running_is_success, running_is_failure, \
-    failure_is_running
-from py_trees_ros.trees import BehaviourTree
+from py_trees import Blackboard
 
-import giskardpy
 import giskardpy.identifier as identifier
-from giskard_msgs.msg import MoveAction, MoveFeedback
-from giskardpy import RobotName
 from giskardpy.data_types import order_map, KeyDefaultDict
 from giskardpy.god_map import GodMap
 from giskardpy.model.collision_world_syncer import CollisionWorldSynchronizer
 from giskardpy.model.world import WorldTree
-from giskardpy.tree.append_zero_velocity import AppendZeroVelocity
-from giskardpy.tree.async_composite import PluginBehavior
-from giskardpy.tree.better_parallel import Parallel, ParallelPolicy
-from giskardpy.tree.cleanup import CleanUp
-from giskardpy.tree.collision_checker import CollisionChecker
-from giskardpy.tree.collision_marker import CollisionMarker
-from giskardpy.tree.collision_scene_updater import CollisionSceneUpdater
-from giskardpy.tree.commands_remaining import CommandsRemaining
-from giskardpy.tree.exception_to_execute import ExceptionToExecute
-from giskardpy.tree.goal_canceled import GoalCanceled
-from giskardpy.tree.goal_reached import GoalReachedPlugin
-from giskardpy.tree.goal_received import GoalReceived
-from giskardpy.tree.instantaneous_controller import ControllerPlugin
-from giskardpy.tree.joint_group_pos_controller_publisher import JointGroupPosController
-from giskardpy.tree.kinematic_sim import KinSimPlugin
-from giskardpy.tree.log_debug_expressions import LogDebugExpressionsPlugin
-from giskardpy.tree.log_trajectory import LogTrajPlugin
-from giskardpy.tree.loop_detector import LoopDetector
-from giskardpy.tree.max_trajectory_length import MaxTrajectoryLength
-from giskardpy.tree.plot_debug_expressions import PlotDebugExpressions
-from giskardpy.tree.plot_trajectory import PlotTrajectory
-from giskardpy.tree.plugin_if import IF
-from giskardpy.tree.publish_feedback import PublishFeedback
-from giskardpy.tree.send_result import SendResult
-from giskardpy.tree.send_trajectory import SendFollowJointTrajectory
-from giskardpy.tree.set_cmd import SetCmd
-from giskardpy.tree.set_error_code import SetErrorCode
-from giskardpy.tree.shaking_detector import WiggleCancel
-from giskardpy.tree.start_timer import StartTimer
-from giskardpy.tree.sync_configuration import SyncConfiguration
-from giskardpy.tree.sync_configuration2 import SyncConfiguration2
-from giskardpy.tree.sync_localization import SyncLocalization
-from giskardpy.tree.tf_publisher import TFPublisher
-from giskardpy.tree.time import TimePlugin
-from giskardpy.tree.tree_manager import TreeManager, render_dot_tree
-from giskardpy.tree.update_constraints import GoalToConstraints
-from giskardpy.tree.visualization import VisualizationBehavior
-from giskardpy.tree.world_updater import WorldUpdater
+from giskardpy.tree.closed_loop_tree import ClosedLoopTree
+from giskardpy.tree.open_loop_tree import OpenLoopTree
 from giskardpy.utils import logging
 from giskardpy.utils.config_loader import ros_load_robot_config
 from giskardpy.utils.math import max_velocity_from_horizon_and_jerk
-from giskardpy.utils.utils import create_path, get_all_classes_in_package
 
 
 def upload_config_file_to_paramserver():
@@ -141,11 +98,11 @@ def check_velocity_limits_reachable(god_map):
                             'with to prediction horizon of \'{}\' '
                             'and jerk limit of \'{}\', '
                             'but limit in urdf/config is \'{}\''.format(joint_name,
-                                                                         velocity_limit_horizon,
-                                                                         prediction_horizon,
-                                                                         jerk_limit,
-                                                                         velocity_limit
-                                                                         ))
+                                                                        velocity_limit_horizon,
+                                                                        prediction_horizon,
+                                                                        jerk_limit,
+                                                                        velocity_limit
+                                                                        ))
             print_help = True
     if print_help:
         logging.logwarn('Check utils.py/max_velocity_from_horizon_and_jerk for help.')
@@ -166,130 +123,10 @@ def let_there_be_motions():
     mode = god_map.get_data(identifier.robot_interface_mode)
     del god_map.get_data(identifier.robot_interface)['mode']
     if mode == 'open_loop':
-        tree = TreeManager(god_map)
+        tree = OpenLoopTree(god_map)
     elif mode == 'closed_loop':
-        tree = grow_tree_closed_loop(god_map)
+        tree = ClosedLoopTree(god_map)
     else:
         raise KeyError('Robot interface mode \'{}\' is not supported.'.format(mode))
 
     return tree
-
-
-def grow_tree_closed_loop(action_server_name, god_map):
-    # ----------------------------------------------
-    # ----------------------------------------------
-    # ----------------------------------------------
-    planning_4 = PluginBehavior('planning IIII', hz=True, sleep=0)
-    action_servers = god_map.get_data(identifier.robot_interface)
-    behaviors = get_all_classes_in_package(giskardpy.tree)
-    for i, (execution_action_server_name, params) in enumerate(action_servers.items()):
-        C = behaviors[params['plugin']]
-        del params['plugin']
-        planning_4.add_plugin(C(execution_action_server_name, **params))
-    planning_4.add_plugin(SyncConfiguration2('update robot configuration', RobotName))
-    planning_4.add_plugin(LogTrajPlugin('log'))
-    if god_map.get_data(identifier.collision_checker) is not None:
-        planning_4.add_plugin(CollisionChecker('collision checker'))
-    # planning_4.add_plugin(VisualizationBehavior('visualization'))
-    # planning_4.add_plugin(CollisionMarker('cpi marker'))
-    planning_4.add_plugin(ControllerPlugin('controller'))
-    planning_4.add_plugin(KinSimPlugin('kin sim'))
-
-    if god_map.get_data(identifier.PlotDebugTrajectory_enabled):
-        planning_4.add_plugin(LogDebugExpressionsPlugin('log lba'))
-    # planning_4.add_plugin(WiggleCancel('wiggle'))
-    # planning_4.add_plugin(LoopDetector('loop detector'))
-    planning_4.add_plugin(GoalReachedPlugin('goal reached'))
-    planning_4.add_plugin(TimePlugin('time'))
-    if god_map.get_data(identifier.MaxTrajectoryLength_enabled):
-        kwargs = god_map.get_data(identifier.MaxTrajectoryLength)
-        planning_4.add_plugin(MaxTrajectoryLength('traj length check', **kwargs))
-    # ----------------------------------------------
-    # ----------------------------------------------
-    planning_3 = Sequence('planning III', sleep=0)
-    planning_3.add_child(planning_4)
-    # planning_3.add_child(running_is_success(TimePlugin)('time for zero velocity'))
-    # planning_3.add_child(AppendZeroVelocity('append zero velocity'))
-    # planning_3.add_child(running_is_success(LogTrajPlugin)('log zero velocity'))
-    # if god_map.get_data(identifier.enable_VisualizationBehavior):
-    #     planning_3.add_child(running_is_success(VisualizationBehavior)('visualization', ensure_publish=True))
-    # if god_map.get_data(identifier.enable_CPIMarker) and god_map.get_data(identifier.collision_checker) is not None:
-    #     planning_3.add_child(running_is_success(CollisionMarker)('collision marker'))
-    # ----------------------------------------------
-    # ----------------------------------------------
-    execute_canceled = Sequence('execute canceled')
-    execute_canceled.add_child(GoalCanceled('goal canceled', action_server_name))
-    execute_canceled.add_child(SetErrorCode('set error code'))
-    publish_result = failure_is_success(Selector)('monitor execution')
-    publish_result.add_child(execute_canceled)
-    # publish_result.add_child(execution_action_server)
-    # ----------------------------------------------
-    # ----------------------------------------------
-    planning_2 = failure_is_success(Selector)('planning II')
-    planning_2.add_child(GoalCanceled('goal canceled', action_server_name))
-    if god_map.get_data(identifier.enable_VisualizationBehavior):
-        planning_2.add_child(running_is_failure(VisualizationBehavior)('visualization'))
-    # if god_map.get_data(identifier.enable_WorldVisualizationBehavior):
-    #     planning_2.add_child(success_is_failure(WorldVisualizationBehavior)('world_visualization'))
-    if god_map.get_data(identifier.enable_CPIMarker) and god_map.get_data(identifier.collision_checker) is not None:
-        planning_2.add_child(running_is_failure(CollisionMarker)('cpi marker'))
-    planning_2.add_child(success_is_failure(StartTimer)('start runtime timer'))
-    planning_2.add_child(planning_3)
-    # ----------------------------------------------
-    move_robot = failure_is_success(Sequence)('move robot')
-    move_robot.add_child(IF('execute?', identifier.execute))
-    move_robot.add_child(publish_result)
-    # ----------------------------------------------
-    # ----------------------------------------------
-    # planning_1 = Sequence('planning I')
-    # ----------------------------------------------
-    planning = failure_is_success(Sequence)('planning')
-    planning.add_child(IF('command set?', identifier.next_move_goal))
-    planning.add_child(GoalToConstraints('update constraints', action_server_name))
-    planning.add_child(planning_2)
-    # planning.add_child(planning_1)
-    # planning.add_child(SetErrorCode('set error code'))
-    if god_map.get_data(identifier.PlotTrajectory_enabled):
-        kwargs = god_map.get_data(identifier.PlotTrajectory)
-        planning.add_child(PlotTrajectory('plot trajectory', **kwargs))
-    if god_map.get_data(identifier.PlotDebugTrajectory_enabled):
-        kwargs = god_map.get_data(identifier.PlotDebugTrajectory)
-        planning.add_child(PlotDebugExpressions('plot debug expressions', **kwargs))
-
-    process_move_cmd = success_is_failure(Sequence)('Process move commands')
-    process_move_cmd.add_child(SetCmd('set move cmd', action_server_name))
-    process_move_cmd.add_child(planning)
-    process_move_cmd.add_child(SetErrorCode('set error code'))
-
-    process_move_goal = failure_is_success(Selector)('Process goal')
-    process_move_goal.add_child(process_move_cmd)
-    process_move_goal.add_child(ExceptionToExecute('clear exception'))
-    process_move_goal.add_child(failure_is_running(CommandsRemaining)('commands remaining?'))
-
-    # ----------------------------------------------
-    # ----------------------------------------------
-    root = Sequence('Giskard')
-    root.add_child(grow_wait_for_goal(action_server_name, god_map))
-    root.add_child(CleanUp('cleanup'))
-    root.add_child(process_move_goal)
-    # root.add_child(move_robot)
-    root.add_child(SendResult('send result', action_server_name, MoveAction))
-
-    return BehaviourTree(root)
-
-
-def grow_wait_for_goal(action_server_name, god_map):
-    wait_for_goal = Sequence('wait for goal')
-    wait_for_goal.add_child(grow_sync_branch(god_map))
-    wait_for_goal.add_child(GoalReceived('has goal', action_server_name, MoveAction))
-    return wait_for_goal
-
-def grow_sync_branch(god_map):
-    sync = Sequence('Synchronize')
-    sync.add_child(WorldUpdater('update world'))
-    sync.add_child(running_is_success(SyncConfiguration)('update robot configuration', RobotName))
-    sync.add_child(SyncLocalization('update robot localization', RobotName))
-    sync.add_child(TFPublisher('publish tf', **god_map.get_data(identifier.TFPublisher)))
-    sync.add_child(CollisionSceneUpdater('update collision scene'))
-    sync.add_child(running_is_success(VisualizationBehavior)('visualize collision scene'))
-    return sync

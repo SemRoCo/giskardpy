cc25652528f4808c22db759032719be5ec909a05
Thomas Lipps <lipps.thomas@web.de>
2022-02-03 11:28:38 +0100

    [tree/retry_planning] check when cartesianpath was followed if the goal specified in there is trivial (aka can be reached with CartesianPose)

--- src/giskardpy/tree/retry_planning.py
+++ src/giskardpy/tree/retry_planning.py
@@ -1,5 +1,7 @@
+import json
 from copy import deepcopy
 
+import actionlib
 import numpy as np
 import rospy
 import yaml
@@ -7,7 +9,8 @@ from py_trees import Status
 
 import giskardpy.identifier as identifier
 import giskardpy.utils.tfwrapper as tf
-from giskard_msgs.msg import Constraint
+from giskard_msgs.msg import Constraint, MoveGoal, MoveAction
+from giskard_msgs.srv import GlobalPathNeededRequest, GlobalPathNeeded
 from giskardpy.exceptions import PlanningException
 from giskardpy.tree.plugin import GiskardBehavior
 from giskardpy.utils import logging
@@ -26,17 +29,73 @@ class RetryPlanning(GiskardBehavior):
         e = self.get_blackboard_exception()
         if e and self.must_replan(e):
             self.clear_blackboard_exception()
-            logging.loginfo(u'Replanning with global planner.')
+            logging.loginfo(u'Replanning a new path.')
             self.get_god_map().set_data(identifier.global_planner_needed, True)
+            return Status.RUNNING
+        elif self.is_reaching_goal_pose_trivial():
+            logging.loginfo(u'CartesianPath terminated early, but goal can be reached trivially.')
+            logging.loginfo(u'Solving rest with CartesianPose.')
+            self.send_trivial_cartesian_pose_goal()
             return Status.RUNNING
         elif self.cartesian_path_planning_failed():
             self.must_replan(PlanningException())
-            logging.loginfo(u'CartesianPath did not follow the the path to the goal.')
-            logging.loginfo(u'Replanning a new path with global planner.')
+            logging.loginfo(u'CartesianPath did not traverse the whole planned path.')
+            logging.loginfo(u'Replanning a new path.')
             self.get_god_map().set_data(identifier.global_planner_needed, True)
             return Status.RUNNING
         else:
             return Status.SUCCESS
+
+    def send_trivial_cartesian_pose_goal(self):
+        c, nc = self.move_parameter_value_pair_to_constraint('CartesianPathCarrot', 'CartesianPose',
+                                                             parameters=['root_link', 'tip_link', 'goal'])
+        move_cmd = self.god_map.get_data(identifier.next_move_goal)  # type: MoveCmd
+        m = MoveGoal()
+        move_cmd.constraints.remove(c)
+        move_cmd.constraints.append(nc)
+        m.cmd_seq.append(move_cmd)
+        m.type = MoveGoal.PLAN_AND_EXECUTE
+        client = actionlib.SimpleActionClient("/giskard/command", MoveAction)
+        client.send_goal(m)
+
+    def move_parameter_value_pair_to_constraint(self, from_type, to_type, parameters=None):
+
+        move_cmd = self.god_map.get_data(identifier.next_move_goal)  # type: MoveCmd
+        if any([c.type in [from_type] for c in move_cmd.constraints]):
+            for c in move_cmd.constraints:
+                if c.type == from_type:
+                    npvps_d = dict()
+                    pvps = yaml.load(c.parameter_value_pair)
+                    for k in parameters:
+                        npvps_d[k] = deepcopy(pvps[k])
+                    nc = Constraint()
+                    nc.type = to_type
+                    nc.parameter_value_pair = json.dumps(npvps_d)
+                    return c, nc
+        else:
+            raise KeyError('Could not find constraint of type {} in move_cmd.'.format(from_type))
+
+    def is_reaching_goal_pose_trivial(self):
+        move_cmd = self.god_map.get_data(identifier.next_move_goal)  # type: MoveCmd
+        if any([c.type in ['CartesianPathCarrot'] for c in move_cmd.constraints]):
+            global_move_cmd = deepcopy(move_cmd)
+            global_move_cmd.constraints = list()
+            for c in move_cmd.constraints:
+                if c.type == 'CartesianPathCarrot':
+                    d = yaml.load(c.parameter_value_pair)
+                    if 'goals' in d:
+                        goal_pose = convert_dictionary_to_ros_message(d['goal']).pose
+                        rospy.wait_for_service('~is_global_path_needed', timeout=5.0)
+                        is_global_path_needed = rospy.ServiceProxy('~is_global_path_needed', GlobalPathNeeded)
+                        req = GlobalPathNeededRequest()
+                        req.root_link = d['root_link']
+                        req.tip_link = d['tip_link']
+                        req.env_group = 'kitchen'
+                        req.pose_goal = goal_pose
+                        req.simple = True
+                        return not is_global_path_needed(req).needed
+                    else:
+                        return False
 
     def cartesian_path_planning_failed(self):
         # TODO: check if goal in c is eq to c.goals[-1]

c93aa6c2d8346d9afe3e1c94ce4f56967aa86e52
Thomas Lipps <lipps.thomas@web.de>
2022-02-17 12:43:12 +0100

    [test/utils] set_object_joint_state: waiting for js until timeout reached

--- test/utils_for_tests.py
+++ test/utils_for_tests.py
@@ -458,31 +458,31 @@ class GiskardTestWrapper(GiskardWrapper):
         logging.loginfo(u'total time spend moving: {}'.format(self.total_time_spend_moving))
         logging.loginfo(u'stopping tree')
 
-    #def set_object_joint_state(self, object_name, joint_state, waiting_time=20, sleep_time=0.1):
-    #    super(GiskardTestWrapper, self).set_object_joint_state(object_name, joint_state)
-    #    i = 0.0
-    #    while not rospy.is_shutdown():
-    #        current_js = self.world.groups[object_name].state
-    #        joint_names_without_prefix = set(j.short_name for j in current_js)
-    #        assert set(joint_state.keys()).difference(joint_names_without_prefix) == set()
-    #        for joint_name, state in current_js.items():
-    #            if joint_name.short_name in joint_state:
-    #                assert i < waiting_time
-    #                if np.isclose(state.position, joint_state[joint_name.short_name], atol=0.01):
-    #                    return
-    #                else:
-    #                    i += sleep_time
-    #                    rospy.sleep(sleep_time)
-
-    def set_object_joint_state(self, object_name, joint_state):
+    def set_object_joint_state(self, object_name, joint_state, waiting_time=20, sleep_time=0.1):
         super(GiskardTestWrapper, self).set_object_joint_state(object_name, joint_state)
-        rospy.sleep(0.5)
-        current_js = self.world.groups[object_name].state
-        joint_names_without_prefix = set(j.short_name for j in current_js)
-        assert set(joint_state.keys()).difference(joint_names_without_prefix) == set()
-        for joint_name, state in current_js.items():
-            if joint_name.short_name in joint_state:
-                np.testing.assert_almost_equal(state.position, joint_state[joint_name.short_name], 2)
+        i = 0.0
+        while not rospy.is_shutdown():
+            current_js = self.world.groups[object_name].state
+            joint_names_without_prefix = set(j.short_name for j in current_js)
+            assert set(joint_state.keys()).difference(joint_names_without_prefix) == set()
+            for joint_name, state in current_js.items():
+                if joint_name.short_name in joint_state:
+                    assert i < waiting_time
+                    if np.isclose(state.position, joint_state[joint_name.short_name], atol=0.01):
+                        return
+                    else:
+                        i += sleep_time
+                        rospy.sleep(sleep_time)
+
+    #def set_object_joint_state(self, object_name, joint_state):
+    #    super(GiskardTestWrapper, self).set_object_joint_state(object_name, joint_state)
+    #    rospy.sleep(0.5)
+    #    current_js = self.world.groups[object_name].state
+    #    joint_names_without_prefix = set(j.short_name for j in current_js)
+    #    assert set(joint_state.keys()).difference(joint_names_without_prefix) == set()
+    #    for joint_name, state in current_js.items():
+    #        if joint_name.short_name in joint_state:
+    #            np.testing.assert_almost_equal(state.position, joint_state[joint_name.short_name], 2)
 
     def set_kitchen_js(self, joint_state, object_name=u'kitchen'):
         self.set_object_joint_state(object_name, joint_state)

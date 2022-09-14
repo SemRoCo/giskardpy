1f692d7f8152aab76bc3ad858495299a8ecd726d
Refills donbot user <refills@homer>
2022-09-14 09:39:07 +0200

    fixed bug in collision blacklist computation and configured donbot

--- scripts/interactive_marker.py
+++ scripts/interactive_marker.py
@@ -185,7 +185,7 @@ class IMServer(object):
                 p.header.frame_id = feedback.header.frame_id
                 p.pose = feedback.pose
                 # self.giskard.set_json_goal('SetPredictionHorizon', prediction_horizon=1)
-                self.giskard.set_cart_goal(root_link=self.root_link,
+                self.giskard.set_straight_cart_goal(root_link=self.root_link,
                                            tip_link=self.tip_link,
                                            goal_pose=p)
 

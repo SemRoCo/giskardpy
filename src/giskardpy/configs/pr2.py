27607773d0872aa53b837571e19691b39aed024e
ichumuh <stelter@uni-bremen.de>
2022-11-02 17:24:51 +0100

    Update pr2.py

--- src/giskardpy/configs/pr2.py
+++ src/giskardpy/configs/pr2.py
@@ -1,4 +1,4 @@
-from giskardpy.configs.data_types import ControlModes
+from giskardpy.configs.data_types import ControlModes, CollisionCheckerLib
 from giskardpy.configs.default_giskard import Giskard
 from giskardpy.my_types import Derivatives
 
@@ -6,6 +6,7 @@ from giskardpy.my_types import Derivatives
 class PR2_Base(Giskard):
     def __init__(self):
         super().__init__()
+        self.set_collision_checker(CollisionCheckerLib.none)
         self.load_moveit_self_collision_matrix('package://giskardpy/config/pr2.srdf')
         self.set_default_external_collision_avoidance(soft_threshold=0.1,
                                                       hard_threshold=0.0)

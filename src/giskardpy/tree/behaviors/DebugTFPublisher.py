ef4d497280cd629170d2c977540db81ca5f6339b
Simon <stelter@uni-bremen.de>
2022-12-05 22:12:54 +0100

    implemented debug marker for frames

--- src/giskardpy/tree/behaviors/DebugTFPublisher.py
+++ src/giskardpy/tree/behaviors/DebugTFPublisher.py
@@ -5,7 +5,7 @@ import rospy
 from geometry_msgs.msg import TransformStamped, Quaternion, Point, Transform
 from py_trees import Status
 from std_msgs.msg import ColorRGBA
-from tf.transformations import quaternion_from_matrix
+from tf.transformations import quaternion_from_matrix, quaternion_about_axis, rotation_matrix
 from tf2_msgs.msg import TFMessage
 from visualization_msgs.msg import Marker, MarkerArray
 
@@ -44,15 +44,83 @@ class DebugTFPublisher(GiskardBehavior):
         ms = []
         for i, (name, value) in enumerate(self.debugs_evaluated.items()):
             expr = self.debugs[name]
-            m = Marker()
-            m.action = m.ADD
-            m.ns = f'debug/{name}'
-            m.id = 0
-            if isinstance(expr, w.Vector3):
-                ref_V_d = value
+            if expr.reference_frame is not None:
+                map_T_ref = self.world.compute_fk_np(self.world.root_link_name, expr.reference_frame)
+            else:
+                map_T_ref = np.eye(4)
+            if isinstance(expr, w.TransMatrix):
+                ref_T_d = value
+                map_T_d = np.dot(map_T_ref, ref_T_d)
+                map_P_d = map_T_d[:4, 3:]
+                # x
+                d_V_x_offset = np.array([width, 0, 0, 0])
+                map_V_x_offset = np.dot(map_T_d, d_V_x_offset)
+                mx = Marker()
+                mx.action = mx.ADD
+                mx.header.frame_id = 'map'
+                mx.ns = f'debug{name}'
+                mx.id = 0
+                mx.type = mx.CYLINDER
+                mx.pose.position.x = map_P_d[0][0] + map_V_x_offset[0]
+                mx.pose.position.y = map_P_d[1][0] + map_V_x_offset[1]
+                mx.pose.position.z = map_P_d[2][0] + map_V_x_offset[2]
+                d_R_x = rotation_matrix(np.pi / 2, [0, 1, 0])
+                map_R_x = np.dot(map_T_d, d_R_x)
+                mx.pose.orientation = Quaternion(*quaternion_from_matrix(map_R_x))
+                mx.color = ColorRGBA(1, 0, 0, 1)
+                mx.scale.x = width / 4
+                mx.scale.y = width / 4
+                mx.scale.z = width * 2
+                ms.append(mx)
+                # y
+                d_V_y_offset = np.array([0, width, 0, 0])
+                map_V_y_offset = np.dot(map_T_d, d_V_y_offset)
+                my = Marker()
+                my.action = my.ADD
+                my.header.frame_id = 'map'
+                my.ns = f'debug{name}'
+                my.id = 1
+                my.type = my.CYLINDER
+                my.pose.position.x = map_P_d[0][0] + map_V_y_offset[0]
+                my.pose.position.y = map_P_d[1][0] + map_V_y_offset[1]
+                my.pose.position.z = map_P_d[2][0] + map_V_y_offset[2]
+                d_R_y = rotation_matrix(-np.pi / 2, [1, 0, 0])
+                map_R_y = np.dot(map_T_d, d_R_y)
+                my.pose.orientation = Quaternion(*quaternion_from_matrix(map_R_y))
+                my.color = ColorRGBA(0, 1, 0, 1)
+                my.scale.x = width / 4
+                my.scale.y = width / 4
+                my.scale.z = width * 2
+                ms.append(my)
+                # z
+                d_V_z_offset = np.array([0, 0, width, 0])
+                map_V_z_offset = np.dot(map_T_d, d_V_z_offset)
+                mz = Marker()
+                mz.action = mz.ADD
+                mz.header.frame_id = 'map'
+                mz.ns = f'debug{name}'
+                mz.id = 2
+                mz.type = mz.CYLINDER
+                mz.pose.position.x = map_P_d[0][0] + map_V_z_offset[0]
+                mz.pose.position.y = map_P_d[1][0] + map_V_z_offset[1]
+                mz.pose.position.z = map_P_d[2][0] + map_V_z_offset[2]
+                # d_R_z = rotation_matrix(np.pi / 2, [0, 1, 0])
+                # map_R_z = np.dot(map_T_d, d_R_z)
+                mz.pose.orientation = Quaternion(*quaternion_from_matrix(map_T_d))
+                mz.color = ColorRGBA(0, 0, 1, 1)
+                mz.scale.x = width / 4
+                mz.scale.y = width / 4
+                mz.scale.z = width * 2
+                ms.append(mz)
+            else:
+                m = Marker()
+                m.action = m.ADD
+                m.ns = f'debug/{name}'
+                m.id = 0
                 m.header.frame_id = 'map'
-                if expr.reference_frame is not None:
-                    map_T_ref = self.world.compute_fk_np(self.world.root_link_name, expr.reference_frame)
+                m.pose.orientation.w = 1
+                if isinstance(expr, w.Vector3):
+                    ref_V_d = value
                     map_T_vis = self.world.compute_fk_np(self.world.root_link_name, expr.vis_frame)
                     map_V_d = np.dot(map_T_ref, ref_V_d)
                     map_P_vis = map_T_vis[:4, 3:]
@@ -60,33 +128,24 @@ class DebugTFPublisher(GiskardBehavior):
                     map_P_p2 = map_P_vis + map_V_d
                     m.points.append(Point(map_P_p1[0][0], map_P_p1[1][0], map_P_p1[2][0]))
                     m.points.append(Point(map_P_p2[0][0], map_P_p2[1][0], map_P_p2[2][0]))
-                else:
-                    map_V_d = ref_V_d
-                    m.points.append(Point())
-                    m.points.append(Point(map_V_d[0][0], map_V_d[1][0], map_V_d[2][0]))
-                m.type = m.ARROW
-                m.color = self.colors[i]
-                m.scale.x = width/2
-                m.scale.y = width
-                m.scale.z = 0
-            elif isinstance(expr, w.Point3):
-                ref_P_d = value
-                m.header.frame_id = 'map'
-                if expr.reference_frame is not None:
-                    map_T_ref = self.world.compute_fk_np(self.world.root_link_name, expr.reference_frame)
+                    m.type = m.ARROW
+                    m.color = self.colors[i]
+                    m.scale.x = width / 2
+                    m.scale.y = width
+                    m.scale.z = 0
+                elif isinstance(expr, w.Point3):
+                    ref_P_d = value
                     map_P_d = np.dot(map_T_ref, ref_P_d)
-                else:
-                    map_P_d = ref_P_d
-                m.pose.position.x = map_P_d[0][0]
-                m.pose.position.y = map_P_d[1][0]
-                m.pose.position.z = map_P_d[2][0]
-                m.pose.orientation.w = 1
-                m.type = m.SPHERE
-                m.color = self.colors[i]
-                m.scale.x = width
-                m.scale.y = width
-                m.scale.z = width
-            ms.append(m)
+                    m.pose.position.x = map_P_d[0][0]
+                    m.pose.position.y = map_P_d[1][0]
+                    m.pose.position.z = map_P_d[2][0]
+                    m.pose.orientation.w = 1
+                    m.type = m.SPHERE
+                    m.color = self.colors[i]
+                    m.scale.x = width
+                    m.scale.y = width
+                    m.scale.z = width
+                ms.append(m)
         return ms
 
     def clear_markers(self):

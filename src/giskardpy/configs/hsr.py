3d0c7774c6169c0c4ac836a756cc5aae85f79cae
malte <huerkamp@uni-bremen.de>
2023-03-13 10:18:01 +0100

    added JointGroupPosition controller to control the hsr arm in closed loop mode via interactive markers

--- src/giskardpy/configs/hsr.py
+++ src/giskardpy/configs/hsr.py
@@ -7,7 +7,10 @@ from giskardpy.configs.default_giskard import Giskard
 class HSR_Base(Giskard):
     def __init__(self, root_link_name: Optional[str] = None):
         super().__init__(root_link_name=root_link_name)
-        self.configure_PlotTrajectory(enabled=True)
+        # self.configure_PlotTrajectory(enabled=True)
+        self.configure_PublishDebugExpressions(enabled=True)
+        self.configure_DebugMarkerPublisher(enabled=True)
+        self.configure_MaxTrajectoryLength(length=30)
         self.load_moveit_self_collision_matrix('package://giskardpy/config/hsrb.srdf')
         self.set_default_external_collision_avoidance(soft_threshold=0.05,
                                                       hard_threshold=0.0)
@@ -84,6 +87,79 @@ class HSR_StandAlone(HSR_Base):
             'wrist_roll_joint',
             'brumbrum'
         ])
+        self.overwrite_external_collision_avoidance(joint_name='brumbrum',
+                                                    number_of_repeller=2,
+                                                    soft_threshold=0.1,
+                                                    hard_threshold=0.03)
+
+
+class HSR_Gazebo(HSR_Base):
+    def __init__(self):
+        self.add_robot_from_parameter_server(joint_state_topics=['/hsrb/joint_states'])
+        super().__init__()
+        self.add_sync_tf_frame('map', 'odom')
+        self.add_omni_drive_joint(parent_link_name='odom',
+                                  child_link_name='base_footprint',
+                                  # odom_x_name='odom_x',
+                                  # odom_y_name='odom_y',
+                                  # odom_yaw_name='odom_t',
+                                  odometry_topic='/hsrb/odom',
+                                  name='brumbrum')
+        self.add_follow_joint_trajectory_server(namespace='/hsrb/head_trajectory_controller/follow_joint_trajectory',
+                                                state_topic='/hsrb/head_trajectory_controller/state',
+                                                fill_velocity_values=True)
+        # self.add_follow_joint_trajectory_server(namespace='/hsrb/omni_base_controller/follow_joint_trajectory',
+        #                                         state_topic='/hsrb/omni_base_controller/state',
+        #                                         fill_velocity_values=True)
+        self.add_follow_joint_trajectory_server(namespace='/hsrb/arm_trajectory_controller/follow_joint_trajectory',
+                                                state_topic='/hsrb/arm_trajectory_controller/state',
+                                                fill_velocity_values=True)
+        self.overwrite_external_collision_avoidance(joint_name='brumbrum',
+                                                    number_of_repeller=2,
+                                                    soft_threshold=0.1,
+                                                    hard_threshold=0.03)
+
+
+class HSR_Local(HSR_Base):
+    def __init__(self):
+        self.add_robot_from_parameter_server(joint_state_topics=['/hsrb/joint_states'])
+        super().__init__()
+        self.add_sync_tf_frame('map', 'odom')
+        self.add_omni_drive_joint(parent_link_name='odom',
+                                  child_link_name='base_footprint',
+                                  odom_x_name='odom_x',
+                                  odom_y_name='odom_y',
+                                  odom_yaw_name='odom_t',
+                                  odometry_topic='/hsrb/odom')
+        self.add_follow_joint_trajectory_server(namespace='/hsrb/head_trajectory_controller/follow_joint_trajectory',
+                                                state_topic='/hsrb/head_trajectory_controller/state',
+                                                fill_velocity_values=True)
+        self.add_follow_joint_trajectory_server(namespace='/hsrb/omni_base_controller/follow_joint_trajectory',
+                                                state_topic='/hsrb/omni_base_controller/state',
+                                                fill_velocity_values=True)
+        self.add_follow_joint_trajectory_server(namespace='/hsrb/arm_trajectory_controller/follow_joint_trajectory',
+                                                state_topic='/hsrb/arm_trajectory_controller/state',
+                                                fill_velocity_values=True)
+        self.overwrite_external_collision_avoidance(joint_name='brumbrum',
+                                                    number_of_repeller=2,
+                                                    soft_threshold=0.1,
+                                                    hard_threshold=0.03)
+
+
+class HSR_GazeboRealtime(HSR_Base):
+    def __init__(self):
+        self.add_robot_from_parameter_server(joint_state_topics=['/hsrb/joint_states'])
+        super().__init__()
+        self.set_control_mode(ControlModes.close_loop)
+        self.add_sync_tf_frame('map', 'odom')
+        self.add_omni_drive_joint(parent_link_name='odom',
+                                  child_link_name='base_footprint',
+                                  # odom_x_name='odom_x',
+                                  # odom_y_name='odom_y',
+                                  # odom_yaw_name='odom_t',
+                                  odometry_topic='/hsrb/odom',
+                                  name='brumbrum')
+        self.add_joint_group_position_controller(namespace='hsrb/realtime_arm_controller')
         self.overwrite_external_collision_avoidance(joint_name='brumbrum',
                                                     number_of_repeller=2,
                                                     soft_threshold=0.1,

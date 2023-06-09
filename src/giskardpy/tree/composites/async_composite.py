bc39c607900ae1fba52b7b5015fc9bded891c605
Simon <stelter@uni-bremen.de>
2023-06-09 18:07:19 +0200

    fixed AsyncBehavior

--- src/giskardpy/tree/composites/async_composite.py
+++ src/giskardpy/tree/composites/async_composite.py
@@ -15,8 +15,6 @@ class AsyncBehavior(GiskardBehavior, Composite):
 
     def __init__(self, name, hz=None):
         super().__init__(name)
-        # TODO why can't we save this in .children?
-        # self._children = OrderedDict()
         self.set_status(Status.INVALID)
         self.status_lock = RLock()
         self.looped_once = False
@@ -26,52 +24,11 @@ class AsyncBehavior(GiskardBehavior, Composite):
         else:
             self.sleeper = None
 
-    # def get_children(self):
-    #     return self._children
-
-    # def add_child(self, child):
-    #     """
-    #     Registers a plugin with the process manager. The name needs to be unique.
-    #     :param child: Behaviour
-    #     :return:
-    #     """
-    #     name = child.name
-    #     if name in self._children:
-    #         raise KeyError(f'A plugin with name \'{name}\' already exists.')
-    #     with self.status_lock:
-    #         self._children[name] = child
-    #
-    # def insert_child(self, child, idx):
-    #     name = child.name
-    #     if name in self._children:
-    #         raise KeyError(f'A plugin with name \'{name}\' already exists.')
-    #     with self.status_lock:
-    #         self._children.
-
-    # def remove_child(self, child_name):
-    #     with self.status_lock:
-    #         del self._children[child_name]
-
-    # def setup(self, timeout):
-        # self.start_children()
-        # return super().setup(timeout)
-    #
-    # def start_children(self):
-    #     for child in self.children:
-    #         child.setup(10.0)
-
     def initialise(self):
         self.looped_once = False
-        with self.status_lock:
-            self.set_status(Status.RUNNING)
         self.update_thread = Thread(target=self.loop_over_plugins)
         self.update_thread.start()
         super().initialise()
-
-    def init_plugins(self):
-        for child in self.children:
-            child.initialise()
-            # plugin.status = Status.RUNNING
 
     def is_running(self):
         return self.status == Status.RUNNING
@@ -92,11 +49,12 @@ class AsyncBehavior(GiskardBehavior, Composite):
         for child in self.children:
             child.stop()
 
-    def update(self):
+    def tick(self):
         with self.status_lock:
-            if not self.update_thread.is_alive():
-                return Status.SUCCESS
-            return self.status
+            if self.status == Status.INVALID:
+                self.status = Status.RUNNING
+                self.initialise()
+            yield self
 
     def set_status(self, new_state):
         self.status = new_state
@@ -107,7 +65,6 @@ class AsyncBehavior(GiskardBehavior, Composite):
     @profile
     def loop_over_plugins(self):
         try:
-            # self.init_plugins()
             self.get_blackboard().runtime = time()
             while self.is_running() and not rospy.is_shutdown():
                 for child in self.children:

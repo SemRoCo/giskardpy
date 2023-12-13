e73705d27f5c9af3cde64d40174c3b6c769b78ff
malte <huerkamp@uni-bremen.de>
2023-12-13 09:34:19 +0100

    rescued the hack

--- src/giskardpy/configs/giskard.py
+++ src/giskardpy/configs/giskard.py
@@ -57,6 +57,7 @@ class Giskard:
             additional_goal_package_paths = set()
         for additional_path in additional_goal_package_paths:
             self.add_goal_package_name(additional_path)
+        god_map.hack = 0
 
     def set_defaults(self):
         god_map.world_config.set_defaults()

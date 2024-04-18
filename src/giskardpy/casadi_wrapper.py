138911602a571284a1097736a3848043757eb33b
Kaviya <kaviya.dhanabalachandran@gmail.com>
2024-04-18 14:44:00 +0200

    remove unnecessary scaling

--- src/giskardpy/casadi_wrapper.py
+++ src/giskardpy/casadi_wrapper.py
@@ -2002,7 +2002,6 @@ def distance_point_to_plane(frame_P_current, frame_P_bottom_left, frame_P_bottom
 
     normal = cross(ab, ac)
     d = normal.dot(frame_P_bottom_left)
-    normal.scale(1)
     normal.scale(d)
     nearest = frame_P_current + normal
     return norm(nearest-frame_P_current), nearest

bfcc54e087bc630cdcb551732c03af75f8345a66
Simon <stelter@uni-bremen.de>
2024-04-22 17:00:38 +0200

    fixed real time pointing

--- src/giskardpy/goals/realtime_goals.py
+++ src/giskardpy/goals/realtime_goals.py
@@ -24,17 +24,22 @@ class RealTimePointing(Pointing):
                  weight: float = WEIGHT_BELOW_CA,
                  start_condition: cas.Expression = cas.TrueSymbol,
                  hold_condition: cas.Expression = cas.FalseSymbol,
-                 end_condition: cas.Expression = cas.TrueSymbol
-                 ):
-        initial_goal = cas.Point3((1,0,1), reference_frame='base_footprint')
+                 end_condition: cas.Expression = cas.TrueSymbol):
+        initial_goal = cas.Point3((1, 0, 1), reference_frame='base_footprint')
         super().__init__(tip_link=tip_link,
                          goal_point=initial_goal,
                          root_link=root_link,
-                         pointing_axis=pointing_axis)
+                         pointing_axis=pointing_axis,
+                         tip_group=tip_group,
+                         root_group=root_group,
+                         max_velocity=max_velocity,
+                         weight=weight,
+                         start_condition=start_condition,
+                         hold_condition=hold_condition,
+                         end_condition=end_condition)
         self.sub = rospy.Subscriber('muh', PointStamped, self.cb)
 
     def cb(self, data: PointStamped):
         data = msg_converter.convert_ros_msg_to_giskard_obj(data, god_map.world)
-        data = god_map.world.transform(self.root, data)
+        data = god_map.world.transform(self.root, data).to_np()
         self.root_P_goal_point = data
-

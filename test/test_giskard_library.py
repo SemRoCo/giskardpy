acf12034b7edaa242a0249067ecd66258df45ff2
Simon <stelter@uni-bremen.de>
2024-04-24 17:37:00 +0200

    test_goal for giskard lib works

--- test/test_giskard_library.py
+++ test/test_giskard_library.py
@@ -1,12 +1,15 @@
+import numpy as np
 import pytest
 
 from giskardpy.data_types.data_types import PrefixName, Derivatives
 from giskardpy.goals import motion_goal_manager
 from giskardpy.goals.joint_goals import JointPositionList
 from giskardpy.goals.motion_goal_manager import MotionGoalManager
+from giskardpy.god_map import god_map
 from giskardpy.model.joints import OmniDrive, PrismaticJoint
 from giskardpy.model.links import Link, BoxGeometry
 from giskardpy.model.world import WorldTree
+from giskardpy.monitors.monitor_manager import MonitorManager
 from giskardpy.qp.qp_controller import QPProblemBuilder
 from giskardpy.symbol_manager import symbol_manager
 
@@ -46,11 +49,11 @@ def box_world(empty_world: WorldTree):
                                axis=(1, 0, 0),
                                lower_limits={Derivatives.position: -10,
                                              Derivatives.velocity: -1,
-                                             Derivatives.acceleration: None,
+                                             Derivatives.acceleration: -np.inf,
                                              Derivatives.jerk: -30},
                                upper_limits={Derivatives.position: 10,
                                              Derivatives.velocity: 1,
-                                             Derivatives.acceleration: None,
+                                             Derivatives.acceleration: np.inf,
                                              Derivatives.jerk: 30})
         world.add_joint(joint)
     assert joint_name in world.joints
@@ -71,12 +74,19 @@ class TestWorld:
 
     def test_goal(self, box_world: WorldTree):
         joint_name = box_world.joint_names[0]
+        box_name = box_world.link_names[-1]
         dt = 0.05
 
         joint_goal = JointPositionList(goal_state={joint_name: 2})
 
+        monitor_manager = MonitorManager()
+        god_map.monitor_manager = monitor_manager
+
         motion_goal_manager = MotionGoalManager()
+        god_map.motion_goal_manager = motion_goal_manager
         motion_goal_manager.add_motion_goal(joint_goal)
+        motion_goal_manager.init_task_state()
+
         eq, neq, neqd, lin_weight, quad_weight = motion_goal_manager.get_constraints_from_goals()
         controller = QPProblemBuilder(sample_period=dt,
                                       free_variables=list(box_world.free_variables.values()),
@@ -85,3 +95,6 @@ class TestWorld:
         substitutions = symbol_manager.resolve_symbols(parameters)
         next_cmd = controller.get_cmd(substitutions)
         box_world.update_state(next_cmd, dt, Derivatives.jerk)
+        box_world.notify_state_change()
+        fk = box_world.compute_fk_point(root=box_world.root_link_name, tip=box_name).to_np()
+        assert fk[0] > 0

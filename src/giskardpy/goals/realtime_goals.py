4c4846ad1d6bd985c5e1c3130c73fd14aa165f24
Simon <stelter@uni-bremen.de>
2024-04-24 10:20:06 +0200

    fixed real time pointing

--- src/giskardpy/goals/realtime_goals.py
+++ src/giskardpy/goals/realtime_goals.py
@@ -5,6 +5,7 @@ from typing import Optional
 import rospy
 from geometry_msgs.msg import PointStamped
 
+from giskardpy.data_types.data_types import PrefixName
 from giskardpy.god_map import god_map
 from giskardpy.tasks.task import WEIGHT_BELOW_CA
 from giskardpy.goals.pointing import Pointing
@@ -15,23 +16,19 @@ import giskardpy.middleware.ros1.msg_converter as msg_converter
 class RealTimePointing(Pointing):
 
     def __init__(self,
-                 tip_link: str,
-                 root_link: str,
+                 tip_link: PrefixName,
+                 root_link: PrefixName,
                  pointing_axis: cas.Vector3,
-                 tip_group: Optional[str] = None,
-                 root_group: Optional[str] = None,
                  max_velocity: float = 0.3,
                  weight: float = WEIGHT_BELOW_CA,
                  start_condition: cas.Expression = cas.TrueSymbol,
                  hold_condition: cas.Expression = cas.FalseSymbol,
                  end_condition: cas.Expression = cas.TrueSymbol):
-        initial_goal = cas.Point3((1, 0, 1), reference_frame='base_footprint')
+        initial_goal = cas.Point3((1, 0, 1), reference_frame=god_map.world.search_for_link_name('base_footprint'))
         super().__init__(tip_link=tip_link,
                          goal_point=initial_goal,
                          root_link=root_link,
                          pointing_axis=pointing_axis,
-                         tip_group=tip_group,
-                         root_group=root_group,
                          max_velocity=max_velocity,
                          weight=weight,
                          start_condition=start_condition,

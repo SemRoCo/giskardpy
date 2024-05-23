d62cea145cf5a9320646815eb1d85c1403314ae1
malte <huerkamp@uni-bremen.de>
2024-05-23 11:02:32 +0200

    launch configuration for kevin standalone

---
+++ scripts/other_robots/kevin/kevin_standalone.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+
+from giskardpy.configs.behavior_tree_config import StandAloneBTConfig
+from giskardpy.configs.giskard import Giskard
+from giskardpy.configs.other_robots.kevin import KevinStandaloneInterface, KevinCollisionAvoidanceConfig
+from giskardpy.configs.world_config import WorldWithDiffDriveRobot
+
+if __name__ == '__main__':
+    rospy.init_node('giskard')
+    giskard = Giskard(world_config=WorldWithDiffDriveRobot(),
+                      collision_avoidance_config=KevinCollisionAvoidanceConfig(),
+                      robot_interface_config=KevinStandaloneInterface(),
+                      behavior_tree_config=StandAloneBTConfig(publish_tf=True, publish_js=True, simulation_max_hz=20))
+    giskard.live()
---
+++ src/giskardpy/configs/other_robots/kevin.py
@@ -0,0 +1,34 @@
+from giskardpy.configs.giskard import CollisionAvoidanceConfig
+from giskardpy.configs.world_config import WorldWithOmniDriveRobot
+from giskardpy.data_types import Derivatives
+from giskardpy.configs.robot_interface_config import StandAloneRobotInterfaceConfig
+
+
+class KevinCollisionAvoidanceConfig(CollisionAvoidanceConfig):
+    def __init__(self, drive_joint_name: str = 'brumbrum'):
+        super().__init__()
+        self.drive_joint_name = drive_joint_name
+
+    def setup(self):
+        # self.collision_scene.collision_checker_id = self.collision_scene.collision_checker_id.none
+        self.load_self_collision_matrix('package://giskardpy/self_collision_matrices/iai/kevin.srdf')
+        self.overwrite_external_collision_avoidance(self.drive_joint_name,
+                                                    number_of_repeller=2,
+                                                    soft_threshold=0.2,
+                                                    hard_threshold=0.1)
+
+
+class KevinStandaloneInterface(StandAloneRobotInterfaceConfig):
+
+    def __init__(self, drive_joint_name: str = 'brumbrum'):
+        super().__init__([
+            drive_joint_name,
+            'robot_left_rocker_joint',
+            'robot_right_rocker_joint',
+            'robot_arm_inner_joint',
+            'robot_arm_outer_joint',
+            'robot_arm_wrist_joint',
+            'robot_arm_column_joint',
+            'robot_arm_gripper_joint',
+            'robot_arm_gripper_mirror_joint'
+        ])

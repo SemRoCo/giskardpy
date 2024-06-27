2ced90d72d2f4188da0b27a1e4ffb390d804602e
malte <huerkamp@uni-bremen.de>
2024-06-27 08:03:54 +0200

    edited standalone launch

--- scripts/iai_robots/stretch/stretch_standalone.py
+++ scripts/iai_robots/stretch/stretch_standalone.py
@@ -6,11 +6,13 @@ from giskardpy.configs.giskard import Giskard
 from giskardpy.configs.iai_robots.stretch import StretchCollisionAvoidanceConfig, StretchStandaloneInterface
 from giskardpy.configs.iai_robots.tiago import TiagoCollisionAvoidanceConfig, TiagoStandaloneInterface
 from giskardpy.configs.world_config import WorldWithDiffDriveRobot
+from giskardpy.configs.qp_controller_config import SupportedQPSolver, QPControllerConfig
 
 if __name__ == '__main__':
     rospy.init_node('giskard')
     giskard = Giskard(world_config=WorldWithDiffDriveRobot(),
                       collision_avoidance_config=StretchCollisionAvoidanceConfig(),
                       robot_interface_config=StretchStandaloneInterface(),
-                      behavior_tree_config=StandAloneBTConfig())
+                      behavior_tree_config=StandAloneBTConfig(publish_tf=True, publish_js=True, simulation_max_hz=20),
+                      qp_controller_config=QPControllerConfig(max_trajectory_length=300))
     giskard.live()

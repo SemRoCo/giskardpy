50bf00f233cb802b8bdc38ae8ae33bfd1072fe89
malte <huerkamp@uni-bremen.de>
2024-07-26 08:31:16 +0200

    kevin scenario

--- test/test_integration_kevin.py
+++ test/test_integration_kevin.py
@@ -94,12 +94,12 @@ class TestCartGoals:
         zero_pose.execute()
 
     def test_scenario(self, zero_pose: KevinTestWrapper):
-        # table_pose = PoseStamped()
-        # table_pose.header.frame_id = 'map'
-        # table_pose.pose.orientation.w = 1
-        # table_pose.pose.position = Point(2, 0, 0.8)
-        # zero_pose.add_box('table', (0.6, 1.2, 0.01), table_pose, 'map')
-        #
+        table_pose = PoseStamped()
+        table_pose.header.frame_id = 'map'
+        table_pose.pose.orientation.w = 1
+        table_pose.pose.position = Point(2, 0, 0.8)
+        zero_pose.add_box('table', (0.6, 1.2, 0.01), table_pose, 'map')
+
         sbs_pose = PoseStamped()
         sbs_pose.header.frame_id = 'map'
         sbs_pose.pose.orientation = Quaternion(*quaternion_from_matrix([[1, 0, 0, 0],
@@ -120,92 +120,92 @@ class TestCartGoals:
         zero_pose.add_mesh_to_world(name='coldplate', mesh='package://giskardpy/test/data/coldplate.stl',
                                     pose=cold_pose,
                                     scale=(0.001, 0.001, 0.001))
-        #
-        # zero_pose.set_joint_goal({'robot_arm_gripper_joint': 0.066})
-        #
-        # base_goal = PoseStamped()
-        # base_goal.header.frame_id = 'map'
-        # base_goal.pose.position = Point(1, 0, 0)
-        # base_goal.pose.orientation.w = 1
-        # zero_pose.set_diff_drive_base_goal(goal_pose=base_goal, tip_link='robot_base_footprint', root_link='map')
-        #
-        # hotel_pose = PoseStamped()
-        # hotel_pose.header.frame_id = 'robot_hotel_nest_17_link'
-        # hotel_pose.pose.orientation = Quaternion(*quaternion_from_matrix([[-1, 0, 0, 0],
-        #                                                                   [0, -1, 0, 0],
-        #                                                                   [0, 0, 1, 0],
-        #                                                                   [0, 0, 0, 1]]))
-        # hotel_pose.pose.position.x = 0.4
-        # zero_pose.motion_goals.add_cartesian_pose(hotel_pose, 'robot_arm_tool_link', 'robot_arm_base_link')
-        #
-        # zero_pose.allow_all_collisions()
-        # zero_pose.execute()
-        #
-        # goal_pose = sbs_pose
-        # goal_pose.pose.orientation.w = 1
-        # goal_pose.pose.orientation.x = 0
-        # goal_pose.pose.orientation.y = 0
-        # goal_pose.pose.orientation.z = 0
-        # goal_pose.pose.position.y -= 0.043
-        # goal_pose.pose.position.z += 0.2
-        # zero_pose.motion_goals.add_cartesian_pose(goal_pose, 'robot_arm_tool_link', 'map')
-        # zero_pose.allow_all_collisions()
-        # zero_pose.execute()
-        # goal_pose.pose.position.z -= 0.19
-        # zero_pose.motion_goals.add_cartesian_pose(goal_pose, 'robot_arm_tool_link', 'map')
-        # zero_pose.allow_all_collisions()
-        # zero_pose.execute()
-        #
-        # zero_pose.set_joint_goal({'robot_arm_gripper_joint': 0.049})  # close
-        # zero_pose.allow_all_collisions()
-        # zero_pose.execute()
-        #
-        # zero_pose.update_parent_link_of_group(name='sbs', parent_link='robot_arm_tool_link')
-        #
-        # goal_pose.pose.position.z += 0.1
-        # zero_pose.motion_goals.add_cartesian_pose(goal_pose, 'robot_arm_tool_link', 'map')
-        # zero_pose.allow_all_collisions()
-        # zero_pose.execute()
-        #
-        # hotel_pose = PoseStamped()
-        # hotel_pose.header.frame_id = 'robot_hotel_nest_17_link'
-        # hotel_pose.pose.orientation = Quaternion(*quaternion_from_matrix([[-1, 0, 0, 0],
-        #                                                                   [0, -1, 0, 0],
-        #                                                                   [0, 0, 1, 0],
-        #                                                                   [0, 0, 0, 1]]))
-        # hotel_pose.pose.position.x = 0.4
-        # zero_pose.motion_goals.add_cartesian_pose(hotel_pose, 'robot_arm_tool_link', 'robot_arm_base_link')
-        # zero_pose.allow_all_collisions()
-        # zero_pose.execute()
-        #
-        # base_goal = PoseStamped()
-        # base_goal.header.frame_id = 'map'
-        # base_goal.pose.position = Point(0.8, 0.4, 0)
-        # base_goal.pose.orientation.w = 1
-        # zero_pose.set_diff_drive_base_goal(goal_pose=base_goal, tip_link='robot_base_footprint', root_link='map')
-        # zero_pose.allow_all_collisions()
-        # zero_pose.execute()
-        #
-        # goal_pose2 = cold_pose
-        # goal_pose2.pose.orientation.w = 1
-        # goal_pose2.pose.orientation.x = 0
-        # goal_pose2.pose.orientation.y = 0
-        # goal_pose2.pose.orientation.z = 0
-        # goal_pose2.pose.position.x -= 0.05
-        # goal_pose2.pose.position.z += 0.2
-        # zero_pose.motion_goals.add_cartesian_pose(goal_pose2, 'robot_arm_tool_link', 'map')
-        # zero_pose.allow_all_collisions()
-        # zero_pose.execute()
-        #
-        # goal_pose2.pose.position.x -= 0.01
-        # goal_pose2.pose.position.z -= 0.12
-        # zero_pose.motion_goals.add_cartesian_pose(goal_pose2, 'robot_arm_tool_link', 'map')
-        # zero_pose.allow_all_collisions()
-        # zero_pose.execute()
-        #
-        # zero_pose.update_parent_link_of_group(name='sbs', parent_link='map')
-        # zero_pose.set_joint_goal({'robot_arm_gripper_joint': 0.066})
-        # goal_pose2.pose.position.z += 0.2
-        # zero_pose.motion_goals.add_cartesian_pose(goal_pose2, 'robot_arm_tool_link', 'map')
-        # zero_pose.allow_all_collisions()
-        # zero_pose.execute()
+
+        zero_pose.set_joint_goal({'robot_arm_gripper_joint': 0.066})
+
+        base_goal = PoseStamped()
+        base_goal.header.frame_id = 'map'
+        base_goal.pose.position = Point(1, 0, 0)
+        base_goal.pose.orientation.w = 1
+        zero_pose.set_diff_drive_base_goal(goal_pose=base_goal, tip_link='robot_base_footprint', root_link='map')
+
+        hotel_pose = PoseStamped()
+        hotel_pose.header.frame_id = 'robot_hotel_nest_17_link'
+        hotel_pose.pose.orientation = Quaternion(*quaternion_from_matrix([[-1, 0, 0, 0],
+                                                                          [0, -1, 0, 0],
+                                                                          [0, 0, 1, 0],
+                                                                          [0, 0, 0, 1]]))
+        hotel_pose.pose.position.x = 0.4
+        zero_pose.motion_goals.add_cartesian_pose(hotel_pose, 'robot_arm_tool_link', 'robot_arm_base_link')
+
+        zero_pose.allow_all_collisions()
+        zero_pose.execute()
+
+        goal_pose = sbs_pose
+        goal_pose.pose.orientation.w = 1
+        goal_pose.pose.orientation.x = 0
+        goal_pose.pose.orientation.y = 0
+        goal_pose.pose.orientation.z = 0
+        goal_pose.pose.position.y -= 0.043
+        goal_pose.pose.position.z += 0.2
+        zero_pose.motion_goals.add_cartesian_pose(goal_pose, 'robot_arm_tool_link', 'map')
+        zero_pose.allow_all_collisions()
+        zero_pose.execute()
+        goal_pose.pose.position.z -= 0.19
+        zero_pose.motion_goals.add_cartesian_pose(goal_pose, 'robot_arm_tool_link', 'map')
+        zero_pose.allow_all_collisions()
+        zero_pose.execute()
+
+        zero_pose.set_joint_goal({'robot_arm_gripper_joint': 0.049})  # close
+        zero_pose.allow_all_collisions()
+        zero_pose.execute()
+
+        zero_pose.update_parent_link_of_group(name='sbs', parent_link='robot_arm_tool_link')
+
+        goal_pose.pose.position.z += 0.1
+        zero_pose.motion_goals.add_cartesian_pose(goal_pose, 'robot_arm_tool_link', 'map')
+        zero_pose.allow_all_collisions()
+        zero_pose.execute()
+
+        hotel_pose = PoseStamped()
+        hotel_pose.header.frame_id = 'robot_hotel_nest_17_link'
+        hotel_pose.pose.orientation = Quaternion(*quaternion_from_matrix([[-1, 0, 0, 0],
+                                                                          [0, -1, 0, 0],
+                                                                          [0, 0, 1, 0],
+                                                                          [0, 0, 0, 1]]))
+        hotel_pose.pose.position.x = 0.4
+        zero_pose.motion_goals.add_cartesian_pose(hotel_pose, 'robot_arm_tool_link', 'robot_arm_base_link')
+        zero_pose.allow_all_collisions()
+        zero_pose.execute()
+
+        base_goal = PoseStamped()
+        base_goal.header.frame_id = 'map'
+        base_goal.pose.position = Point(0.8, 0.4, 0)
+        base_goal.pose.orientation.w = 1
+        zero_pose.set_diff_drive_base_goal(goal_pose=base_goal, tip_link='robot_base_footprint', root_link='map')
+        zero_pose.allow_all_collisions()
+        zero_pose.execute()
+
+        goal_pose2 = cold_pose
+        goal_pose2.pose.orientation.w = 1
+        goal_pose2.pose.orientation.x = 0
+        goal_pose2.pose.orientation.y = 0
+        goal_pose2.pose.orientation.z = 0
+        goal_pose2.pose.position.x -= 0.05
+        goal_pose2.pose.position.z += 0.2
+        zero_pose.motion_goals.add_cartesian_pose(goal_pose2, 'robot_arm_tool_link', 'map')
+        zero_pose.allow_all_collisions()
+        zero_pose.execute()
+
+        goal_pose2.pose.position.x -= 0.01
+        goal_pose2.pose.position.z -= 0.12
+        zero_pose.motion_goals.add_cartesian_pose(goal_pose2, 'robot_arm_tool_link', 'map')
+        zero_pose.allow_all_collisions()
+        zero_pose.execute()
+
+        zero_pose.update_parent_link_of_group(name='sbs', parent_link='map')
+        zero_pose.set_joint_goal({'robot_arm_gripper_joint': 0.066})
+        goal_pose2.pose.position.z += 0.2
+        zero_pose.motion_goals.add_cartesian_pose(goal_pose2, 'robot_arm_tool_link', 'map')
+        zero_pose.allow_all_collisions()
+        zero_pose.execute()

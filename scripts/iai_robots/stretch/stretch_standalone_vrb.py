60bf45be1fbbb106a34f8afc005b025b4d7bddb5
malte <huerkamp@uni-bremen.de>
2024-08-16 12:50:27 +0200

    stretch vrb vis collision mesh

--- scripts/iai_robots/stretch/stretch_standalone_vrb.py
+++ scripts/iai_robots/stretch/stretch_standalone_vrb.py
@@ -6,7 +6,7 @@ from giskardpy.configs.giskard import Giskard
 from giskardpy.configs.iai_robots.stretch import StretchCollisionAvoidanceConfig, StretchStandaloneInterface
 from giskardpy.configs.world_config import WorldWithDiffDriveRobot
 from giskardpy.configs.qp_controller_config import QPControllerConfig, SupportedQPSolver
-
+from giskardpy.model.ros_msg_visualization import VisualizationMode
 
 if __name__ == '__main__':
     rospy.init_node('giskard')
@@ -14,6 +14,7 @@ if __name__ == '__main__':
                       collision_avoidance_config=StretchCollisionAvoidanceConfig(),
                       robot_interface_config=StretchStandaloneInterface(),
                       behavior_tree_config=StandAloneBTConfig(publish_js=False, publish_tf=True, simulation_max_hz=20,
-                                                              debug_mode=True),
+                                                              debug_mode=True,
+                                                              visualization_mode=VisualizationMode.CollisionsDecomposedFrameLocked),
                       qp_controller_config=QPControllerConfig(qp_solver=SupportedQPSolver.qpSWIFT))
     giskard.live()

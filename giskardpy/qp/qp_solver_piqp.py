595fb4d4ce706a2e63cf842dc42bd2b062ecea80
Simon Stelter <stelter@uni-bremen.de>
2024-11-03 00:40:49 +0100

    implemented piqp

--- giskardpy/qp/qp_solver_piqp.py
+++ giskardpy/qp/qp_solver_piqp.py
@@ -17,13 +17,21 @@ class QPSolverPIQP(QPSolverGurobi):
     solver_id = SupportedQPSolver.piqp
     sparse = True
 
+    def analyze_infeasibility(self):
+        pass
+
     @profile
     def solver_call(self, H: np.ndarray, g: np.ndarray, E: sp.csc_matrix, b: np.ndarray, A: sp.csc_matrix,
                     lb: np.ndarray, ub: np.ndarray, h: np.ndarray) -> np.ndarray:
-        H = sp.diags(H, offsets=0, format='csc')
-        solver = piqp.SparseSolver()
 
-        solver.setup(P=H, c=g, A=E, b=b, G=A, h=h, x_lb=lb, x_ub=ub)
+        H = sp.diags(H+self.regularization_value*10, offsets=0, format='csc')
+        solver = piqp.SparseSolver()
+        # solver.settings.eps_abs = 1e-3
+        # solver.settings.eps_rel = 1e-4
+        if len(h) == 0:
+            solver.setup(P=H, c=g, A=E, b=b, G=None, h=None, x_lb=lb, x_ub=ub)
+        else:
+            solver.setup(P=H, c=g, A=E, b=b, G=A, h=h, x_lb=lb, x_ub=ub)
 
         status = solver.solve()
         if status.value != piqp.PIQP_SOLVED:

634a5ba5e78c9048dfa24a58e393d8dff343dbc3
Simon Stelter <stelter@uni-bremen.de>
2024-11-22 18:10:53 +0100

    debug expressions have a style now

--- giskardpy/debug_expression_manager.py
+++ giskardpy/debug_expression_manager.py
@@ -1,7 +1,9 @@
-from typing import Dict, Optional, List
+from copy import deepcopy
+from typing import Dict, Optional, List, Tuple, Union
 
 import numpy as np
 import pandas as pd
+from data_types.data_types import Derivatives
 from line_profiler import profile
 
 import giskardpy.casadi_wrapper as cas
@@ -28,9 +30,15 @@ class DebugExpressionManager:
         self.compiled_debug_expressions = {}
         self.evaluated_debug_expressions = {}
 
-    def add_debug_expression(self, name: str, expression: cas.Expression, color: Optional[ColorRGBA] = None):
+    def add_debug_expression(self, name: str, expression: cas.Expression, color: Optional[ColorRGBA] = None,
+                             derivative: Derivatives = Derivatives.position,
+                             derivatives_to_plot: Optional[List[Derivatives]] = None):
+        if derivatives_to_plot is None:
+            derivatives_to_plot = [derivative]
         if isinstance(expression, cas.Symbol_):
             expression.color = color
+        expression.debug_derivative = derivative
+        expression.debug_derivatives_to_plot = derivatives_to_plot
         self.debug_expressions[PrefixName(name, prefix='')] = expression
 
     def compile_debug_expressions(self):
@@ -62,39 +70,49 @@ class DebugExpressionManager:
         if len(self.evaluated_debug_expressions) > 0:
             self._raw_debug_trajectory.append(self.evaluated_debug_expressions)
 
-    def raw_traj_to_traj(self) -> Trajectory:
+    def raw_traj_to_traj(self, control_dt: float) -> Trajectory:
         debug_trajectory = Trajectory()
         for control_cycle_counter, evaluated_debug_expressions in enumerate(self._raw_debug_trajectory):
-            last_mjs = None
             if control_cycle_counter >= 1:
                 last_mjs = debug_trajectory.get_exact(control_cycle_counter - 1)
-            js = JointStates()
+            else:
+                last_mjs = JointStates()
+            js = deepcopy(last_mjs)
             for name, value in evaluated_debug_expressions.items():
                 if len(value) > 1:
                     if len(value.shape) == 2:
                         for x in range(value.shape[0]):
                             for y in range(value.shape[1]):
                                 tmp_name = f'{name}|{x}_{y}'
-                                self.evaluated_expr_to_js(tmp_name, last_mjs, js, value[x, y])
+                                self.evaluated_expr_to_js(tmp_name, last_mjs, js, float(value[x, y]), control_dt,
+                                                          self.debug_expressions[name].debug_derivative)
                     else:
                         for x in range(value.shape[0]):
                             tmp_name = f'{name}|{x}'
-                            self.evaluated_expr_to_js(tmp_name, last_mjs, js, value[x])
+                            self.evaluated_expr_to_js(tmp_name, last_mjs, js, float(value[x]), control_dt,
+                                                      self.debug_expressions[name].debug_derivative)
                 else:
-                    self.evaluated_expr_to_js(name, last_mjs, js, value)
+                    self.evaluated_expr_to_js(name, last_mjs, js, float(value), control_dt,
+                                              self.debug_expressions[name].debug_derivative)
             debug_trajectory.set(control_cycle_counter, js)
+        for control_cycle_counter, evaluated_debug_expressions in enumerate(self._raw_debug_trajectory):
+            js = debug_trajectory.get_exact(control_cycle_counter)
+            for name in self.debug_expressions:
+                for d in Derivatives.range(Derivatives.position, Derivatives.jerk):
+                    if d not in self.debug_expressions[name].debug_derivatives_to_plot:
+                        js[name][d] = 0
+
         return debug_trajectory
 
-    def evaluated_expr_to_js(self, name, last_js, next_js: JointStates, value):
-        if last_js is not None:
-            velocity = value - last_js[name].position
-        else:
-            if isinstance(value, np.ndarray):
-                velocity = np.zeros(value.shape)
-            else:
-                velocity = 0
-        next_js[name].position = value
-        next_js[name].velocity = velocity / god_map.qp_controller.sample_period
+    def evaluated_expr_to_js(self, name: Union[PrefixName, str], last_js: JointStates, next_js: JointStates, value: float,
+                             dt:float, derivative: Derivatives):
+        next_js[name][derivative] = value
+
+        for i in range(derivative + 1, 4):
+            next_js[name][i] = (next_js[name][i - 1] - last_js[name][i - 1]) / dt
+
+        for i in range(derivative - 1, -1, -1):
+            next_js[name][i] = next_js[name][i] + next_js[name][i + 1] * dt
 
     def to_pandas(self):
         p_debug = {}

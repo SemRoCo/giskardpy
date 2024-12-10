55224118621a32c767a2e0f42aa8104086ee0d98
malte <huerkamp@uni-bremen.de>
2024-12-10 15:13:59 +0100

    second kevin standalone launch

---
+++ scripts/other_robots/kevin/kevin_standalone2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import rospy
+
+from giskardpy.configs.behavior_tree_config import StandAloneBTConfig, VisualizationMode
+from giskardpy.configs.giskard import Giskard
+from giskardpy.configs.other_robots.kevin import KevinStandaloneInterface, KevinCollisionAvoidanceConfig, \
+    WorldWithKevinAndFixedArmsRobot, KevinAndArmsStandaloneInterface
+from giskardpy.configs.world_config import WorldWithDiffDriveRobot
+
+if __name__ == '__main__':
+    rospy.init_node('giskard')
+    giskard = Giskard(world_config=WorldWithDiffDriveRobot(),
+                      collision_avoidance_config=KevinCollisionAvoidanceConfig(),
+                      robot_interface_config=KevinStandaloneInterface(),
+                      behavior_tree_config=StandAloneBTConfig(publish_tf=True, publish_js=True, simulation_max_hz=20,
+                                                              visualization_mode=VisualizationMode.VisualsFrameLocked))
+    giskard.live()

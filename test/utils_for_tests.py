1562d2ed904adc36a5f05575c52038170e1ee84d
Simon Stelter <stelter@uni-bremen.de>
2025-01-16 17:56:32 +0100

    move diffdrive tasks from goals to tasks

--- test/utils_for_tests.py
+++ test/utils_for_tests.py
@@ -29,7 +29,7 @@ from giskard_msgs.srv import DyeGroupResponse
 from giskardpy.data_types.data_types import KeyDefaultDict
 from giskardpy.data_types.data_types import PrefixName, Derivatives
 from giskardpy.data_types.exceptions import UnknownGroupException, DuplicateNameException, WorldException
-from giskardpy.motion_statechart.goals.diff_drive_goals import DiffDriveTangentialToPoint, KeepHandInWorkspace
+from giskardpy.motion_statechart.tasks.diff_drive_goals import DiffDriveTangentialToPoint, KeepHandInWorkspace
 from giskardpy.god_map import god_map
 from giskardpy.middleware import get_middleware
 from giskardpy.model.collision_world_syncer import Collisions, Collision, CollisionEntry

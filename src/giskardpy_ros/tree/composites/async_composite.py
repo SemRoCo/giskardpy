6144854d42a7537477fc63377e8a5ef40aabdb0f
Simon Stelter <stelter@uni-bremen.de>
2025-04-07 12:16:35 +0200

    fixed terminate of async composite, when there is no max_hz

--- src/giskardpy_ros/tree/composites/async_composite.py
+++ src/giskardpy_ros/tree/composites/async_composite.py
@@ -40,7 +40,8 @@ class AsyncBehavior(GiskardBehavior, Composite):
         return self.status == Status.RUNNING
 
     def terminate(self, new_status: Status) -> None:
-        get_middleware().loginfo(f'avg dt was {self.sleeper.avg_dt}')
+        if self.sleeper is not None:
+            get_middleware().loginfo(f'avg dt was {self.sleeper.avg_dt}')
         self.set_status(Status.FAILURE)
         try:
             self.update_thread.join()

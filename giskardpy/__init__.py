12f5f22f3a16ac97b5c869c39196f35e704604c0
Simon Stelter <stelter@uni-bremen.de>
2025-04-10 10:36:02 +0200

    removed networkx import

--- giskardpy/__init__.py
+++ giskardpy/__init__.py
@@ -5,8 +5,6 @@ def preload_matplotlib():
     import matplotlib.pyplot
     from scipy import sparse
     import pandas
-    from networkx.algorithms.shortest_paths.generic import shortest_path
-    from networkx.drawing.nx_pydot import read_dot
     from pkg_resources import resource_filename
 
 # Start preloading in the background

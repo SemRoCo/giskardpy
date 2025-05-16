736efc57df656c1842bc3f17f523765d2fe927e9
Simon Stelter <stelter@uni-bremen.de>
2025-05-16 11:17:38 +0200

    implemented JointPositionAbove

--- giskardpy/motion_statechart/monitors/joint_monitors.py
+++ giskardpy/motion_statechart/monitors/joint_monitors.py
@@ -1,9 +1,10 @@
 from typing import Dict, Optional
 
 import giskardpy.casadi_wrapper as cas
+from giskardpy.data_types.exceptions import GoalInitalizationException
 from giskardpy.motion_statechart.monitors.monitors import Monitor
 from giskardpy.god_map import god_map
-from giskardpy.data_types.data_types import Derivatives
+from giskardpy.data_types.data_types import Derivatives, PrefixName
 
 
 class JointGoalReached(Monitor):
@@ -22,4 +23,17 @@ class JointGoalReached(Monitor):
             comparison_list.append(cas.less(cas.abs(error), threshold))
         expression = cas.logic_all(cas.Expression(comparison_list))
         super().__init__(name=name)
+        self.observation_expression = expression
+
+
+class JointPositionAbove(Monitor):
+    def __init__(self,
+                 joint_name: PrefixName,
+                 threshold: float,
+                 name: Optional[str] = None):
+        super().__init__(name=name)
+        current = god_map.world.get_one_dof_joint_symbol(joint_name, Derivatives.position)
+        if god_map.world.is_joint_continuous(joint_name):
+            raise GoalInitalizationException(f'{self.__class__.__name__} does not support joints of type continuous.')
+        expression = cas.greater(current, threshold)
         self.observation_expression = expression
